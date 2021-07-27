dddab1e9ebd8f74be2c2a4b53aa2031b2d1c5ee8
Rishikesh <rishikesh11599@gmail.com>
2021-07-27 22:17:25 +0530

    changed get header

--- src/main/java/com/example/springboot/config/SecurityConfig.java
+++ src/main/java/com/example/springboot/config/SecurityConfig.java
@@ -45,7 +45,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/user/**","/auth/register","/Create_event","/feed/get")
+        http.csrf().disable().authorizeRequests().antMatchers("/user/**","/auth/register","/Create_event",
+                "/event/getall","/event/get/**","/notice/**","/feed/get","/")
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
